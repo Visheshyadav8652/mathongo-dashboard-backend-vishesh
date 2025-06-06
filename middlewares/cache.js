b9597a531144f100d1efe2ba10d62c5141f050c3
Visheshyadav8652 <162328787+Visheshyadav8652@users.noreply.github.com>
2025-06-06 20:31:26 +0530

    implemented proper caching of the request

--- middlewares/cache.js
+++ middlewares/cache.js
@@ -6,11 +6,13 @@ module.exports = async (req, res, next) => {
   try {
     const cached = await redisClient.get(key);
     if (cached) {
+      console.log('Serving chapters from Redis cache for key:', key);
       return res.json(JSON.parse(cached));
     }
     // Monkey-patch res.json to cache the response
     const originalJson = res.json.bind(res);
     res.json = (data) => {
+      console.log('Caching chapters in Redis for key:', key);
       redisClient.setEx(key, 3600, JSON.stringify(data)); // 1 hour
       originalJson(data);
     };
--- routes/chapterRoutes.js
+++ routes/chapterRoutes.js
@@ -7,7 +7,10 @@ const upload = multer();
 const cache = require('../middlewares/cache');
 
 // GET all chapters (with filters, pagination, and cache)
-router.get('/', cache, chapterController.getAllChapters);
+router.get('/', cache, (req, res, next) => {
+  console.log('GET /api/v1/chapters called');
+  next();
+}, chapterController.getAllChapters);
 
 // GET a specific chapter by ID
 router.get('/:id', chapterController.getChapterById);
